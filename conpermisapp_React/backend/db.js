bba3ad1609be38bec4515f845a23d794658d5134
Nico <nvh.develop@gmail.com>
2025-01-28 23:09:07 -0300

    Update db.js

--- conpermisapp_React/backend/db.js
+++ conpermisapp_React/backend/db.js
@@ -32,8 +32,14 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT, 
+  dialect: 'postgres', 
   logging: false, // Desactiva logs en producción
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // 🔹 Necesario en Render para evitar errores SSL
+    },
+  },
 });
 
 const connectDB = async () => {
